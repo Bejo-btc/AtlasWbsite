cda1d93d873892797bf0a4ff8a525887ff52863b
Bejo-btc <bejobtc7@gmail.com>
2026-05-03 07:55:09 +0300

    add icons

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -15,16 +15,16 @@ function Landing() {
           </Link>
           <div className='cards'>
             <div className="card">
-              <span >180+</span>
-              <p>يعد القطاع العقاري من أهم القطاعات الاقتصاديه </p>
+              <span><i className="fa-solid fa-user-tie"></i></span>
+              <p>كلعميل عنده قصه مختلفه </p>
             </div>
             <div className="card">
-              <span >180+</span>
-              <p>يعد القطاع العقاري من أهم القطاعات الاقتصاديه </p>
+               <span><i className="fa-solid fa-house-circle-check"></i></span>
+              <p>كل عقار لازم يلقى الشخص المناسب </p>
             </div>
             <div className="card">
-              <span >180+</span>
-              <p>يعد القطاع العقاري من أهم القطاعات الاقتصاديه </p>
+              <span><i className="fa-solid fa-handshake"></i></span>
+              <p> الوضوح والصدق اساس في التعامل  </p>
             </div>
           </div>
         </div>
